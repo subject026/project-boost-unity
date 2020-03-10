21a4e490febe06aa632a25530485a1cd1b70603b
subject026 <sudobuildspeakers@gmail.com>
2020-03-10 01:03:15 +0000

    added some materials keybindings and a sound clip

---
+++ Assets/Scenes/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    void Start() {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update() {
+        ProcessInput();
+    }
+
+    private void ProcessInput() {
+        if (Input.GetKey(KeyCode.Space)) {
+            print("spacebar pressed");
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) {
+                audioSource.Play();
+            }
+        } else {
+            if (audioSource.isPlaying) {
+                audioSource.Stop();
+            }
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            print("left key pressed");
+            transform.Rotate(Vector3.forward);
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            print("right key pressed");
+            transform.Rotate(Vector3.back);
+        }
+    }
+}
